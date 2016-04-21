06b92cd87078ecf1da08e14296f22253ef11f9d0
Jurden <jordan.millett@maine.edu>
2016-04-21 02:05:44 -0400

    test code

--- test_main.c
+++ test_main.c
@@ -1,29 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 #include "encoder.h"
 #include "crc16.h"
 #include "ax25.h"
 //#include "afsk.h"
-int test_ax25(char *data);
+int test_afsk(char *callsign, char *data);
+int test_lock();
+int test_query();
+int test_set(int delim_n);
 int main(int argc, char *argv[])
 {
+	int test;
+	int ret;
 	if(argc == 1) {
 		printf("Jordan's Test Code\n");
-		printf("Usage: 'cmd' 'arg'(if any)\n");
-		printf("	cmd		arg\n");
-		printf("1 - Test AX25, 		data\n");
-		printf("2 - Test locking, 	no arg\n");
-		printf("3 - ioctl Query, 	no arg\n");
-		printf("4 - ioctl Set, 		# of delims\n");
+		printf("Usage: 'cmd' 'arg'\n");
+		printf("cmd			arg(s)\n");
+		printf(" 1 - Test afsk, 	callsign, data\n");
+		printf(" 2 - Test locking, 	no arg\n");
+		printf(" 3 - ioctl Query, 	no arg\n");
+		printf(" 4 - ioctl Set, 	# of delims\n");
 		return 0;
-	}/* 
-	switch (argv[2]) {
+	}
+	test = atoi(argv[1]);
+	switch (test) {
 		case 1:
-			if(argc == 3) {
+			if(argc != 4) {
+				printf("Include data for afsk\n");
+				return -1;
+			}
+			ret = test_afsk(argv[2], argv[3]);
+			return 0;
+		case 2:
+			if(argc != 2) {
+				printf("Test doesn't require arg\n");
+				return -1;
+			}
+			return 0;
+		case 3:
+			if(argc != 2) {
+				printf("Test doesn't require arg\n");
+				return -1;
+			}
+			return 0;
+		case 4:
+			if(argc != 3) {
+				printf("Include number of delims\n");
+				return -1;
+			}
+			return 0;
+	}
+}
+int test_afsk(char *callsign, char *data) {
+	int fd;
+	int size;
+	int ret;
+	
+	fd = open("/dev/afsk",O_WRONLY);
+	printf("%d\n",fd);
 
-			printf("
-			*/
+		
+	return 0;
+}
+int test_lock() {
+	return 0;
+}
+int test_query() {
+	return 0;
+}
+int test_set(int delim_n) {
+	return 0;	
 }
