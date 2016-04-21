59bbef012432a572212fbed930f1065202acf8d8
Jurden <jordan.millett@maine.edu>
2016-04-21 02:07:32 -0400

    More includes

--- test_main.c
+++ test_main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
 
 #include "encoder.h"
 #include "crc16.h"
